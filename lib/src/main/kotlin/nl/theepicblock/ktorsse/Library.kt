a4700fa3b971e2445a653869266134013d4f514b
TheEpicBlock <git.teb@theepicblock.nl>
2025-02-05 22:43:51 +0100

    Allow for control over retry delay

--- lib/src/main/kotlin/nl/theepicblock/ktorsse/Library.kt
+++ lib/src/main/kotlin/nl/theepicblock/ktorsse/Library.kt
@@ -5,8 +5,9 @@ import io.ktor.client.request.*
 import io.ktor.sse.*
 import io.ktor.utils.io.*
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
-import kotlin.time.Duration
 
 fun HttpClient.launchSseListener(urlString: String? = null, builder: SseListenerBuilder.() -> Unit): Job {
     return launch {
@@ -17,6 +18,8 @@ fun HttpClient.launchSseListener(urlString: String? = null, builder: SseListener
 suspend fun HttpClient.createSseListener(urlString: String? = null, builder: SseListenerBuilder.() -> Unit) {
     val builderData = SseListenerBuilder()
     builder(builderData)
+    var delay = builderData.defaultReconnectionTime?.toMillis()
+    var retryCount = 0
 
     val statement = prepareRequest {
         if (urlString != null) {
@@ -24,62 +27,82 @@ suspend fun HttpClient.createSseListener(urlString: String? = null, builder: Sse
         }
         builderData.requestBuilder?.let { it(this) }
     }
-    statement.body<ByteReadChannel, Unit> { channel ->
-        builderData.onConnected()
-        // SSE parsing.
-        // See: https://www.w3.org/TR/2012/WD-eventsource-20120426/#parsing-an-event-stream
-        val data = StringBuilder()
-        val comment = StringBuilder()
-        var event: String? = null
-        var id: String? = null // last event ID
-        var retry: Long? = null
+    while (isActive) {
+        statement.body<ByteReadChannel, Unit> { channel ->
+            builderData.onConnected()
+            retryCount = 0
+            // SSE parsing.
+            // See: https://www.w3.org/TR/2012/WD-eventsource-20120426/#parsing-an-event-stream
+            val data = StringBuilder()
+            val comment = StringBuilder()
+            var event: String? = null
+            var id: String? = null // last event ID
+            var retry: Long? = null
 
-        while (true) {
-            var line = channel.readUTF8Line()
-            if (line == null) break
+            while (isActive) {
+                var line = channel.readUTF8Line()
+                if (line == null) break
 
-            if (line.isEmpty()) {
-                // Dispatch the event
-                builderData.listener(ServerSentEvent(
-                    data = data.toString(),
-                    event = event,
-                    comments = comment.toString(),
-                    retry = retry,
-                    id = id,
-                ))
-                data.clear()
-                comment.clear()
-                event = null
-                retry = null
-            } else {
-                val colonIndex = line.indexOf(':')
-
-                var field: String
-                var value: String
-                if (colonIndex == -1) {
-                    // the line is not empty but does not contain a U+003A COLON character (:)
-                    // According to spec we must interpret the line as a field, and leave the value empty
-                    field = line
-                    value = ""
+                if (line.isEmpty()) {
+                    // Dispatch the event
+                    if (retry != null) {
+                        delay = retry
+                    }
+                    builderData.listener(
+                        ServerSentEvent(
+                            data = data.toString(),
+                            event = event,
+                            comments = comment.toString(),
+                            retry = retry,
+                            id = id,
+                        )
+                    )
+                    data.clear()
+                    comment.clear()
+                    event = null
+                    retry = null
                 } else {
-                    // the line contains a U+003A COLON character
-                    field = line.substring(0, colonIndex)
-                    var valueStart = colonIndex+1
-                    if (valueStart < line.length && line[valueStart] == ' ') valueStart++
-                    value = line.substring(valueStart, line.length)
-                }
+                    val colonIndex = line.indexOf(':')
 
-                // Process the field/value pair
-                when (field) {
-                    "event" -> event = value
-                    "data" -> data.append(value)
-                    "id" -> id = value
-                    "retry" -> value.toLongOrNull()?.let { retry = it }
-                    // We'll also record comment fields, to match ktor's native sse handling
-                    "" -> comment.append(value)
-                    // Ignore any other values
+                    var field: String
+                    var value: String
+                    if (colonIndex == -1) {
+                        // the line is not empty but does not contain a U+003A COLON character (:)
+                        // According to spec we must interpret the line as a field, and leave the value empty
+                        field = line
+                        value = ""
+                    } else {
+                        // the line contains a U+003A COLON character
+                        field = line.substring(0, colonIndex)
+                        var valueStart = colonIndex + 1
+                        if (valueStart < line.length && line[valueStart] == ' ') valueStart++
+                        value = line.substring(valueStart, line.length)
+                    }
+
+                    // Process the field/value pair
+                    when (field) {
+                        "event" -> event = value
+                        "data" -> data.append(value)
+                        "id" -> id = value
+                        "retry" -> value.toLongOrNull()?.let { retry = it }
+                        // We'll also record comment fields, to match ktor's native sse handling
+                        "" -> comment.append(value)
+                        // Ignore any other values
+                    }
                 }
             }
+        }
+        builderData.onDisconnected()
+        val retryData = RetryHandler(delay, retryCount)
+        builderData.retryHandler(retryData)
+        retryCount++
+        if (!retryData.answered) {
+            throw IllegalStateException("Must call retry() or dontRetry()")
+        }
+        if (retryData.retryMillis == -1L) {
+            break
+        } else {
+            delay(retryData.retryMillis)
         }
     }
 }
@@ -88,5 +111,33 @@ class SseListenerBuilder {
     var requestBuilder: (HttpRequestBuilder.() -> Unit)? = null
     var listener: ((ServerSentEvent) -> Unit) = {}
     var onConnected: (() -> Unit) = {}
-    var defaultReconnectionTime: Duration? = null
+    var onDisconnected: (() -> Unit) = {}
+    var defaultReconnectionTime: java.time.Duration? = null
+    var retryHandler: (RetryHandler.() -> Unit) = {
+        retry(retryDelay ?: 5_000)
+    }
+}
+
+class RetryHandler(
+    /**
+     * The amount of time that the server has requested we delay the retry for
+     */
+    val retryDelay: Long?,
+    /**
+     * Incremented for each concurrent retry. Will be zero on the first retry
+     */
+    val retryCount: Int
+) {
+    internal var answered: Boolean = false
+    internal var retryMillis: Long = -1
+
+    fun retry(milliseconds: Long) {
+        answered = true
+        retryMillis = milliseconds
+    }
+
+    fun dontRetry() {
+        answered = true
+        retryMillis = -1
+    }
 }
--- lib/src/test/kotlin/nl/theepicblock/ktorsse/SseClientTests.kt
+++ lib/src/test/kotlin/nl/theepicblock/ktorsse/SseClientTests.kt
@@ -5,12 +5,15 @@ import io.ktor.sse.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.trySendBlocking
+import java.time.Duration
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.time.Duration
+import kotlin.test.assertTrue
+import kotlin.test.assertFalse
 
 class SseClientTests {
-    @Test fun basicSse() {
+    @Test
+    fun basicSse() {
         runBlocking {
             withTimeout(2000) {
                 SseTestServer().use { server ->
@@ -29,9 +32,10 @@ class SseClientTests {
         }
     }
 
-    @Test fun reconnect() {
+    @Test
+    fun reconnect() {
         runBlocking {
-            withTimeout(2000) {
+            withTimeout(5000) {
                 val server = SseTestServer()
                 val client = HttpClient()
                 val listener = testListener(client, server.url)
@@ -44,10 +48,12 @@ class SseClientTests {
 
                 // Restart server
                 server.close()
+                listener.awaitDisconnection()
                 val server2 = SseTestServer(server.port)
                 listener.awaitConnected()
                 assertNoData(listener)
                 assertEquals(1, server2.listenerCount)
+                server2.send("Test2")
                 assertEquals("Test2", listener.awaitNextEvent().data)
                 server2.close()
 
@@ -61,12 +67,18 @@ class SseClientTests {
         val conEvents = Channel<Boolean>()
         val job = client.launchSseListener(url) {
             onConnected = {
+                println("Listener: connected")
                 conEvents.trySendBlocking(true)
             }
+            onDisconnected = {
+                println("Listener: disconnected")
+                conEvents.trySendBlocking(false)
+            }
             listener = {
+                println("Listener: received event")
                 sseEvents.trySendBlocking(it)
             }
-            defaultReconnectionTime = Duration.parse("1s")
+            defaultReconnectionTime = Duration.ofMillis(200)
         }
         return TestListener(sseEvents, conEvents, job)
     }
@@ -75,9 +87,19 @@ class SseClientTests {
         listener.assertNoData()
     }
 
-    class TestListener(private var sseEvents: Channel<ServerSentEvent>, private val connectionEvents: Channel<Boolean>, val job: Job) {
+    class TestListener(
+        private var sseEvents: Channel<ServerSentEvent>,
+        private val connectionEvents: Channel<Boolean>,
+        val job: Job
+    ) {
         suspend fun awaitConnected() {
-            while (!connectionEvents.receive()) {}
+            val next = connectionEvents.receive()
+            assertTrue(next)
+        }
+
+        suspend fun awaitDisconnection() {
+            val next = connectionEvents.receive()
+            assertFalse(next)
         }
 
         suspend fun awaitNextEvent(): ServerSentEvent {
